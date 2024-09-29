4f8a0f09edad3215d4dfe0c6deda232379fbf248
root <root@localhost>
2024-09-30 01:15:55 +0800

    auto publish 1727630155722

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById('magicButton').addEventListener('click', function() {
-    alert('WARNING: You`re currently downloading a trojan virus');
+    alert('WARNING: You`re currently downloading a trojan virus,do you want to continue downloading?');
     alert('Magaan na ba ang iyong paghinga?');
     alert('bumalik ka na sakin');
     alert('i miss you');
